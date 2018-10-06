63e55019dc6873ca92e7f4acc973ee4ae693cce6
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-10-06 14:01:40 -0500

    add h1 to main

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-// homework 5
+// homework 5 step 3
 
 var main = document.createElement('main');
 var pageBody = document.getElementsByTagName('body')[0];
 var scriptTag = document.getElementsByTagName('script')[0];
 
 pageBody.insertBefore(main, scriptTag);
+
+// step 4
+
+var header = document.createElement('h1');
+main.appendChild(header);
