12209c7ba3ad0178f110675ba5018aa6aa4e1d1e
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-10-07 08:00:40 -0500

    add blue to header

--- main.js
+++ main.js
@@ -30,3 +30,7 @@ var listItemText = document.createTextNode('This is the list item');
 
 header.appendChild(headerText);
 items.appendChild(listItemText);
+
+// step 11
+
+header.classList.add('blue');
